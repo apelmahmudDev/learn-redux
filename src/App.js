8070294f71579b45ea8205f800847a442ac1921d
apelmahmudDev <apelmahmud.dev@gmail.com>
2020-11-20 09:21:16 +0800

    Redux basic in diffrent angle

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
+import { useSelector, useDispatch } from 'react-redux';
+import { decrement, increment, signIn } from './actions';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+	const counter = useSelector((state) => state.counter);
+  const isLogged = useSelector((state) => state.isLogged);
+  const dispatch = useDispatch();
+
+	return (
+		<div className="App">
+			<h1>Counter {counter}</h1>
+			<button onClick={() => dispatch(increment(5))}>+</button>
+			<button onClick={() => dispatch(decrement())}>-</button>
+
+			{isLogged ? <h2>Valuable information i shouldn't see</h2> : ''}
+      <br/>
+      <button onClick={() => dispatch(signIn())}>Sign In</button>
+		</div>
+	);
 }
 
 export default App;
